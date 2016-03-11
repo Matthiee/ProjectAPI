fbe8ed824075384d8e23e3ffd873498b5567b23a
Matthias Beerens <beerens.matthias@hotmail.com>
2016-03-11 09:56:08 +0100

    Aanmaken projectAPI

---
+++ src/java/resources/Configuration.java
@@ -0,0 +1,13 @@
+package resources;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ * @author Matthias
+ */
+@ApplicationPath("api")
+public class Configuration extends Application{
+    
+}
